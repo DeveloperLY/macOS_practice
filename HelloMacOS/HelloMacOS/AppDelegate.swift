d8254249540710b79b0c717f572dcf528cbf33e4
LY <coderyliu@gmail.com>
2022-06-12 00:16:05 +0800

    feat: 模态窗口

--- HelloMacOS/HelloMacOS/AppDelegate.swift
+++ HelloMacOS/HelloMacOS/AppDelegate.swift
@@ -12,11 +12,18 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
-
-
+    @IBOutlet weak var modalWindow: NSWindow!
+    
+    @IBAction func showModalWindowButtonDidClick(_ sender: NSButton) {
+        NSApplication.shared.runModal(for: self.modalWindow)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         // 做一些应用启动前的初始化处理
+        
+        // 监听窗口关闭事件
+        NotificationCenter.default.addObserver(self, selector: #selector(self.windowClose(_:)), name: NSWindow.willCloseNotification, object: nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,6 +35,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
+    // 退出模态窗口状态
+    @objc func windowClose(_ window: NSWindow) {
+        NSApplication.shared.stopModal()
+    }
 
 }
 
